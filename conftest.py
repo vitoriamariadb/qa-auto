450e54015cd529e2baf812f3d1f8b2bbbb9ed0a6
Vitória Maria <vitoriamaria.sds@gmail.com>
2021-08-10 14:30:00 -0300

    feat: adiciona framework pytest

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "api: testes de API REST"
+    )
+    config.addinivalue_line(
+        "markers", "ui: testes de interface UI"
+    )
+    config.addinivalue_line(
+        "markers", "slow: testes lentos"
+    )
+    config.addinivalue_line(
+        "markers", "smoke: testes de fumaça"
+    )
---
+++ tests/test_sample.py
@@ -0,0 +1,13 @@
+import pytest
+
+@pytest.mark.smoke
+def test_exemplo():
+    assert 1 + 1 == 2
+
+def test_string():
+    assert "qa-auto".upper() == "QA-AUTO"
+
+def test_lista():
+    dados = [1, 2, 3]
+    assert len(dados) == 3
+    assert 2 in dados
