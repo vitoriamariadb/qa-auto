0fb6fec3d175e44a643e9f009c248ec071983cff
Vitória Maria <vitoriamaria.sds@gmail.com>
2021-09-28 15:30:00 -0300

    feat: adiciona parametrizacao testes

---
+++ tests/test_parametrized.py
@@ -0,0 +1,46 @@
+import pytest
+
+@pytest.mark.parametrize("num1,num2,resultado", [
+    (1, 1, 2),
+    (2, 3, 5),
+    (10, 5, 15),
+    (100, 200, 300),
+])
+def test_soma(num1, num2, resultado):
+    assert num1 + num2 == resultado
+
+@pytest.mark.parametrize("texto,esperado", [
+    ("hello", "HELLO"),
+    ("world", "WORLD"),
+    ("python", "PYTHON"),
+    ("testing", "TESTING"),
+])
+def test_uppercase(texto, esperado):
+    assert texto.upper() == esperado
+
+@pytest.mark.parametrize("lista,tamanho", [
+    ([1, 2, 3], 3),
+    ([1, 2, 3, 4, 5], 5),
+    ([], 0),
+    ([1], 1),
+])
+def test_tamanho_lista(lista, tamanho):
+    assert len(lista) == tamanho
+
+@pytest.mark.api
+@pytest.mark.parametrize("user_id", [1, 2, 3, 4, 5])
+def test_get_multiple_users(http_session, api_base_url, user_id):
+    response = http_session.get(f"{api_base_url}/users/{user_id}")
+    assert response.status_code == 200
+    user = response.json()
+    assert user["id"] == user_id
+
+@pytest.mark.parametrize("status_code,is_success", [
+    (200, True),
+    (201, True),
+    (400, False),
+    (404, False),
+    (500, False),
+])
+def test_status_code_validation(status_code, is_success):
+    assert (200 <= status_code < 300) == is_success
---
+++ tests/test_parametrized_api.py
@@ -0,0 +1,27 @@
+import pytest
+
+@pytest.mark.api
+@pytest.mark.parametrize("endpoint", [
+    "/users",
+    "/posts",
+    "/comments",
+    "/albums",
+    "/photos",
+    "/todos"
+])
+def test_endpoint_accessibility(http_session, api_base_url, endpoint):
+    response = http_session.get(f"{api_base_url}{endpoint}")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) > 0
+
+@pytest.mark.api
+@pytest.mark.parametrize("post_id,expected_comments", [
+    (1, 5),
+    (2, 5),
+])
+def test_post_comments_count(http_session, api_base_url, post_id, expected_comments):
+    response = http_session.get(f"{api_base_url}/posts/{post_id}/comments")
+    assert response.status_code == 200
+    comments = response.json()
+    assert len(comments) == expected_comments
